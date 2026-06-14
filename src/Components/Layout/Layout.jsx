26540508254a90c9f13151f2135d04d63f1d4040
Kris Wong <74948841+uwuzumaki@users.noreply.github.com>
2026-06-13 22:31:20 -0400

    Fixed scaling sizing problem

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -6,10 +6,10 @@ const Layout = () => {
   return (
     <>
       <HeaderBar />
-      <div className="flex flex-1 flex-col-reverse sm:flex-row">
+      <div className="flex flex-1 flex-col-reverse justify-center sm:flex-row">
         <Navigation />
-        <div className="flex max-w-7xl flex-1 flex-col items-center sm:mx-0">
-          <div className="mb-1 w-full min-w-90 sm:max-w-90 sm:min-w-146">
+        <div className="flex max-w-7xl flex-1 flex-col items-center sm:mx-0 sm:max-w-146">
+          <div className="mb-1 w-full min-w-90 overflow-hidden">
             <Outlet />
           </div>
         </div>
--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -8,14 +8,19 @@ const Profile = () => {
     <>
       <div className="flex flex-col items-start">
         <div>displayname</div>
+        <div className="flex w-full flex-row justify-between">
+          <div>test</div>
+          <div>test2</div>
+        </div>
         <div>username</div>
         <div className="flex">
           <div>followers</div>
           <div>following</div>
           <div>posts</div>
+          <div>Follow requests</div>
         </div>
         <div>description</div>
-        <div className="mb-1 w-full min-w-90 sm:max-w-90 sm:min-w-146">
+        <div className="mb-1 w-full min-w-90 sm:max-w-146 sm:min-w-90">
           <Card />
         </div>
       </div>
