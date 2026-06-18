dfcc71f2db53239d93bf38f3964225b1510f013d
Kris Wong <74948841+uwuzumaki@users.noreply.github.com>
2026-06-18 00:16:24 -0400

    Likes header bar context

--- src/Components/Likes/Likes.jsx
+++ src/Components/Likes/Likes.jsx
@@ -1,6 +1,14 @@
+import { use, useEffect } from "react";
 import Card from "../../ui/Card";
+import { PageContext } from "../../contexts/PageContext";
 
 const Likes = () => {
+  const title = use(PageContext);
+
+  useEffect(() => {
+    title.setPageTile("Likes");
+  });
+
   return (
     <>
       <Card />
