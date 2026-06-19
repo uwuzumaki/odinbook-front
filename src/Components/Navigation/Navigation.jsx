5cb485b70cbe8824fe58944d8b3c8a4c4c718c3f
Kris Wong <74948841+uwuzumaki@users.noreply.github.com>
2026-06-18 22:19:01 -0400

    Added icon framework for nav bar

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,14 +1,33 @@
 import { Link } from "react-router-dom";
+import Icon from "../../ui/Icon";
 
 const Navigation = () => {
   return (
     <div className="w-min-[25vw] border-color-amber-50 flex flex-row justify-between border sm:flex-col sm:justify-center">
-      <Link to="/">Home</Link>
-      <Link to="search">Search</Link>
-      <Link to="create">Create</Link>
-      <Link to="likes">Likes</Link>
-      <Link to="allUsers">Users</Link>
-      <Link to="u/user">Profile</Link>
+      <Link className="flex" to="/">
+        <Icon icon="iHome" />
+        <div className="hidden md:block">Home</div>
+      </Link>
+      <Link className="flex" to="search">
+        <Icon icon="iSearch" />
+        <div className="hidden md:block">Search</div>
+      </Link>
+      <Link className="flex" to="create">
+        <Icon icon="iCreate" />
+        <div className="hidden md:block">Create</div>
+      </Link>
+      <Link className="flex" to="likes">
+        <Icon icon="iLikes" />
+        <div className="hidden md:block">Likes</div>
+      </Link>
+      <Link className="flex" to="allUsers">
+        <Icon icon="iUsers" />
+        <div className="hidden md:block">Users</div>
+      </Link>
+      <Link className="flex" to="u/user">
+        <Icon icon="iProfile" />
+        <Link className="hidden md:block">Profile</Link>
+      </Link>
     </div>
   );
 };
---
+++ src/ui/Icon.jsx
@@ -0,0 +1,11 @@
+const Icon = ({ icon }) => {
+  return (
+    <>
+      <div>
+        <p>{icon}</p>
+      </div>
+    </>
+  );
+};
+
+export default Icon;
