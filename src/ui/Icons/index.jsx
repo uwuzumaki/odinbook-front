7e6aa217f9943be67f84b0697cf1756b9c93ab79
Kris Wong <74948841+uwuzumaki@users.noreply.github.com>
2026-06-20 17:00:53 -0400

    Added some svg icons for nav bar

---
+++ src/ui/Icons/index.jsx
@@ -0,0 +1,6 @@
+import HomeIcon from "./HomeIcon";
+import SearchIcon from "./SearchIcon";
+import CreateIcon from "./CreateIcon";
+import LikeIcon from "./LikeIcon";
+
+export { HomeIcon, SearchIcon, CreateIcon, LikeIcon };
