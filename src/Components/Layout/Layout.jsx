608a77d4761598253c08a90666d9bd036a9bbd94
Kris Wong <74948841+uwuzumaki@users.noreply.github.com>
2026-06-21 00:32:04 -0400

    Fixed responsive design for layout and navbar

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -7,7 +7,7 @@ import { PageProvider } from "../../contexts/PageContext";
 const Layout = () => {
   return (
     <>
-      <div className="flex flex-1 flex-col-reverse justify-center sm:flex-row">
+      <div className="flex flex-1 flex-col-reverse justify-center bg-white sm:flex-row">
         <Navigation />
         <div className="flex max-w-7xl flex-1 flex-col items-center sm:mx-0 sm:max-w-146">
           <div className="mb-1 w-full min-w-90 overflow-hidden">
--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -10,30 +10,30 @@ import {
 
 const Navigation = () => {
   return (
-    <div className="w-min-[25vw] flex flex-row justify-between pt-4 sm:mx-auto sm:flex-col sm:items-start sm:justify-start">
+    <div className="w-min-[25vw] flex flex-row justify-between pt-4 sm:flex-col sm:items-start sm:justify-start sm:pr-6 lg:pr-12">
       <NavLink location="/">
         <HomeIcon />
-        <div className="hidden md:block">Home</div>
+        <div className="hidden lg:block">Home</div>
       </NavLink>
       <NavLink to="search">
         <SearchIcon />
-        <div className="hidden md:block">Search</div>
+        <div className="hidden lg:block">Search</div>
       </NavLink>
       <NavLink to="create">
         <CreateIcon />
-        <div className="hidden md:block">Create</div>
+        <div className="hidden lg:block">Create</div>
       </NavLink>
       <NavLink to="likes">
         <LikeIcon />
-        <div className="hidden md:block">Likes</div>
+        <div className="hidden lg:block">Likes</div>
       </NavLink>
       <NavLink to="allUsers">
         <UserIcon />
-        <div className="hidden md:block">Users</div>
+        <div className="hidden lg:block">Users</div>
       </NavLink>
       <NavLink to="u/user">
         {/* <Icon icon="iProfile" /> */}
-        <div className="hidden md:block">Profile</div>
+        <div className="hidden lg:block">Profile</div>
       </NavLink>
     </div>
   );
