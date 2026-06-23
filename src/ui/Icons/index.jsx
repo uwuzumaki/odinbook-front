1ee43eeac6db212a72981e852be12dc5bfbf8d18
Kris Wong <74948841+uwuzumaki@users.noreply.github.com>
2026-06-22 23:34:30 -0400

    Added file icon for image upload

--- src/ui/Icons/index.jsx
+++ src/ui/Icons/index.jsx
@@ -3,5 +3,6 @@ import SearchIcon from "./SearchIcon";
 import CreateIcon from "./CreateIcon";
 import LikeIcon from "./LikeIcon";
 import UserIcon from "./UserIcon";
+import FileIcon from "./FileIcon";
 
-export { HomeIcon, SearchIcon, CreateIcon, LikeIcon, UserIcon };
+export { HomeIcon, SearchIcon, CreateIcon, LikeIcon, UserIcon, FileIcon };
