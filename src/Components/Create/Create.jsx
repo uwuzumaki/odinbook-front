a75250bd5de8b8134215f0a111b8f5e3cafdb88a
Kris Wong <74948841+uwuzumaki@users.noreply.github.com>
2026-06-22 23:46:38 -0400

    Updated stylin for create page

--- src/Components/Create/Create.jsx
+++ src/Components/Create/Create.jsx
@@ -1,5 +1,6 @@
 import { useState, use, useEffect } from "react";
 import { PageContext } from "../../contexts/PageContext";
+import { FileIcon } from "../../ui/Icons";
 
 // after creating the post go to the post
 // bsky layout
@@ -24,12 +25,31 @@ const Create = () => {
   };
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <label>create</label>
-        <input onChange={handleChange} value={input} />
-        {/* Replace input with an icon or something */}
-        <input type="file" />
-        <button type="submit">123</button>
+      <form
+        className="flex flex-col border-b border-gray-200"
+        onSubmit={handleSubmit}
+      >
+        <textarea
+          className="h-32 w-full resize-none p-2 focus:outline-none"
+          onChange={handleChange}
+          value={input}
+          placeholder="What's on your mind?"
+        />
+        <div className="flex justify-between">
+          <label
+            className="m-2 rounded-3xl p-2 hover:cursor-pointer hover:bg-blue-200"
+            htmlFor="file-upload"
+          >
+            <FileIcon />
+          </label>
+          <input type="file" id="file-upload" className="hidden" />
+          <button
+            className="m-2 rounded-4xl bg-blue-600 px-3 py-1 text-white hover:cursor-pointer hover:bg-blue-800"
+            type="submit"
+          >
+            Post
+          </button>
+        </div>
       </form>
     </>
   );
