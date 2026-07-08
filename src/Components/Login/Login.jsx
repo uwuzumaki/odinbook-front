565c204cfbf228d70c7d5806fc07ae5824c372f2
Kris Wong <74948841+uwuzumaki@users.noreply.github.com>
2026-07-07 22:32:27 -0400

    Styling for login page

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -5,27 +7,52 @@ const Login = () => {
 
   return (
     <>
-      <div className="flex h-full w-full flex-col items-center justify-center">
-        <div className="flex w-full max-w-2/3 flex-col items-center p-4">
-          <div className="mb-4 text-2xl font-bold">Sign In</div>
-          <form className="flex w-full flex-col" onSubmit={handleSubmit}>
-            <input
-              className="mb-2 rounded-xl bg-gray-200 p-2 focus:bg-blue-200"
-              placeholder="Username"
-            />
-            <input
-              className="rounded-xl bg-gray-200 p-2 focus:bg-blue-200"
-              placeholder="Password"
-            />
-            <button
-              className="mt-2 rounded-xl bg-blue-600 py-2 text-white hover:cursor-pointer hover:bg-blue-800"
-              type="submit"
-            >
+      <div className="flex h-screen w-full flex-1 flex-col items-center justify-center md:flex-row">
+        <div className="hidden flex-1 items-end md:flex md:flex-col md:justify-center">
+          <div className="pb-4 text-right text-4xl font-semibold text-blue-600">
+            Sign In
+          </div>
+          <div className="text-right text-xs lg:text-sm xl:text-base">
+            Enter your username and password
+          </div>
+          <div className="text-right text-xs lg:text-sm xl:text-base">
+            Or use one of the other available options.
+          </div>
+        </div>
+        <div className="flex h-full w-full flex-col items-start justify-center p-6 md:flex-2">
+          <div className="flex w-full flex-2 flex-col items-center justify-center p-6 lg:max-w-2xl">
+            <div className="mb-4 w-full text-left text-2xl font-bold md:hidden">
               Sign In
-            </button>
-          </form>
-          <div>or</div>
-          <div>Continue with Github</div>
+            </div>
+            <form className="flex w-full flex-col" onSubmit={handleSubmit}>
+              <input
+                className="mb-2 rounded-xl bg-gray-200 p-2 focus:bg-blue-200"
+                placeholder="Username"
+              />
+              <input
+                className="rounded-xl bg-gray-200 p-2 focus:bg-blue-200"
+                placeholder="Password"
+              />
+              <button
+                className="mt-2 rounded-xl bg-blue-600 py-1 text-white hover:cursor-pointer hover:bg-blue-800"
+                type="submit"
+              >
+                Sign In
+              </button>
+            </form>
+            <div className="my-2">or</div>
+            <div className="w-full rounded-xl bg-slate-950 py-1 text-white hover:cursor-pointer hover:bg-slate-700">
+              Continue with Github
+            </div>
+          </div>
+          <div className="flex w-full p-6 lg:max-w-2xl">
+            <Link
+              to="/register"
+              className="w-full rounded-xl bg-emerald-600 p-1 text-white hover:bg-emerald-800"
+            >
+              Register
+            </Link>
+          </div>
         </div>
       </div>
     </>
