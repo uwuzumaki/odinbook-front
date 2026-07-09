dbd54b54eb0f8e3ca78282ca28abefa35ba51414
Kris Wong <74948841+uwuzumaki@users.noreply.github.com>
2026-07-08 23:21:46 -0400

    Added authprovider to outlet

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -3,20 +3,23 @@ import Navigation from "../Navigation/Navigation";
 import HeaderBar from "../HeaderBar/HeaderBar";
 
 import { PageProvider } from "../../contexts/PageContext";
+import { AuthProvider } from "../../contexts/AuthContext";
 
 const Layout = () => {
   return (
     <>
       <div className="flex flex-1 flex-col-reverse justify-center bg-white sm:flex-row">
-        <Navigation />
-        <div className="flex max-w-7xl flex-1 flex-col items-center border-r border-l border-slate-200 sm:mx-0 sm:max-w-146">
-          <div className="mb-1 w-full min-w-90 overflow-hidden">
-            <PageProvider>
-              <HeaderBar />
-              <Outlet />
-            </PageProvider>
+        <AuthProvider>
+          <Navigation />
+          <div className="flex max-w-7xl flex-1 flex-col items-center border-r border-l border-slate-200 sm:mx-0 sm:max-w-146">
+            <div className="mb-1 w-full min-w-90 overflow-hidden">
+              <PageProvider>
+                <HeaderBar />
+                <Outlet />
+              </PageProvider>
+            </div>
           </div>
-        </div>
+        </AuthProvider>
       </div>
     </>
   );
