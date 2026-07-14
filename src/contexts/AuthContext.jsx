9506c4f50ba188b82133b4b53fad309edc3e1c6d
Kris Wong <74948841+uwuzumaki@users.noreply.github.com>
2026-07-13 22:08:43 -0400

    Added loader data to context

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,13 +1,32 @@
 import axios from "axios";
 import { createContext, useContext, useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
+  const loaderData = useLoaderData();
   const [username, setUsername] = useState();
   const [authenticated, setAuthenticated] = useState(false);
 
-  return <AuthContext>{children}</AuthContext>;
+  useEffect(() => {
+    try {
+      setUsername(loaderData.user.username);
+      console.log(loaderData);
+      loaderData ? setAuthenticated(true) : null;
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  const value = {
+    username,
+    setUsername,
+    authenticated,
+    setAuthenticated,
+  };
+
+  return <AuthContext value={value}>{children}</AuthContext>;
 };
 
 export { AuthContext, AuthProvider };
