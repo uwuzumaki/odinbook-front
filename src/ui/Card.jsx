eacf27df9244b24b80e562d16119170d5ba35c88
Kris Wong <74948841+uwuzumaki@users.noreply.github.com>
2026-07-15 22:17:39 -0400

    Card styling

--- src/ui/Card.jsx
+++ src/ui/Card.jsx
@@ -1,20 +1,27 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const Card = ({ post }) => {
   return (
-    <div className="-mx-px -mt-px border border-gray-200 p-4 hover:cursor-pointer hover:bg-slate-50">
+    <Link
+      to={`/p/${post.id}`}
+      className="-mx-px -mt-px block border border-gray-200 p-4 hover:cursor-pointer hover:bg-slate-50"
+    >
       <div className="flex justify-between">
         <div className="flex">
-          <div className="mr-4">{post.postAuthor.displayName}</div>
-          <div>@{post.postAuthor.username}</div>
+          <div className="mr-1 h-8 w-8 rounded-full border">img</div>
+          <div className="mr-2">{post.postAuthor.displayName}</div>
+          <div className="text-gray-600">@{post.postAuthor.username}</div>
         </div>
-        <div>{post.createdAt}</div>
+        <div className="text-gray-600">{post.createdAt}</div>
       </div>
-      <div className="mt-4 flex">{post.content}</div>
+      <div className="mt-4 flex text-left">{post.content}</div>
       <div>Image</div>
-      <div className="flex">
-        <div>likes {post.like.length}</div>
-        <div>comments {post.comments.length}</div>
+      <div className="flex text-gray-600">
+        <div className="text-sm">likes {post.like.length}</div>
+        <div className="text-sm">comments {post.comments.length}</div>
       </div>
-    </div>
+    </Link>
   );
 };
 
